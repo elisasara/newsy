171f9f834c2ebd8b3c3f7d4abfea33d661ae6c16
Elisa Urda <urda.elisa@gmail.com>
2018-06-10 19:30:20 -0400

    working on cheerio call from button click. Not working right now

---
+++ public/js/script.js
@@ -0,0 +1,19 @@
+var axios = require("axios");
+// $(document).ready(function () {
+    $("#scrape").on("click", function () {
+        console.log("I've been clicked!");
+        axios.get("https://www.theatlantic.com/")
+            .then(function (response) {
+                var $ = cheerio.load(response.data);
+
+                $(".o-hed").each(function (i, element) {
+                    var result = {};
+
+                    result.title = $(this).children("a").text();
+                    result.link = $(this).children("a").attr("href");
+
+                    console.log(result);
+                })
+            })
+    });
+// });
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,23 +1,41 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-// var axios = require("axios");
+var axios = require("axios");
 var cheerio = require("cheerio");
 var app = express();
 
 
 // // Use body-parser for handling form submissions
 // app.use(bodyParser.urlencoded({ extended: true }));
-module.exports = function(app) {
-app.get("/", function(req, res){
-    // var articleObj = {};
+module.exports = function (app) {
+    app.get("/", function (req, res) {
+        // var articleObj = {};
 
-    // on click, clear db and make axios call for article scraping
-    // push article info into db
-    // create object to be passed into handlebars file
+        // on click, clear db and make axios call for article scraping
+        // $("#scrape").on("click", function () {
+        //     axios.get("https://www.theatlantic.com/")
+        //         .then(function (response) {
+        //             var $ = cheerio.load(response.data);
 
-    // render the object to the index.handlebars
-    res.render("index");
-});
+        //             $(".o-hed").each(function (i, element) {
+        //                 var result = {};
+
+        //                 result.title = $(this).children("a").text();
+        //                 result.link = $(this).children("a").attr("href");
+                        
+        //                 console.log(result);
+        //             })
+        //         })
+                // .then(function(result){
+                //     console.log(resul)
+                // });
+        // });
+        // push article info into db
+        // create object to be passed into handlebars file
+
+        // render the object to the index.handlebars
+        res.render("index");
+    });
 
 };
