52bb8e0c92209ea20366249d2eef78e031819e23
Elisa Urda <urda.elisa@gmail.com>
2018-06-10 17:29:37 -0400

    working on file structure

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
+// Does this line connecto DB?
+mongoose.connect("mongodb://localhost/newsyDb");
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
+
