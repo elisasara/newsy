ea0136b04a8eedb7222eaed371dc57178ce36802
Elisa Urda <urda.elisa@gmail.com>
2018-06-16 18:57:44 -0400

    why won't my note populate to the correct article

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,5 @@
-$(document).ready(function () {
-
-
+$(document).ready(function(){
+    
     $(".save").on("click", function () {
         console.log("Save has been clicked");
         var id = $(event.target).attr("data-id");
@@ -16,10 +15,25 @@ $(document).ready(function () {
 
     $(".addNote").on("click", function (event) {
         console.log("Add Note has been clicked!");
+        var id = $(this).attr("data-id");
         $(".noteArea").css("visibility", "visible");
+        $("#submitNote").attr({"data-id": id});
+    //     $("#noteForm").html("<form class='noteArea'>" +
+    //     "<div class='form-group'>" +
+    //         "<label for='title'>Title</label>" +
+    //         "<br />" +
+    //         "<input type='text' id='title'>" + 
+    //     "</div>" + 
+    //     "<div class='form-group'>" + 
+    //         "<label for='note'>Note</label>" + 
+    //         "<br />" + 
+    //         "<textarea type='text' id='note'></textarea>" + 
+    //     "</div>" + 
+    //     "<button class='btn btn-success' id='submitNote " + "data-id=" + id + "type='submit'>Add Note</button>" + 
+    // "</form>");
     });
 
-    $(".submitNote").on("click", function(event){
+    $("#submitNote").on("click", function(event){
         event.preventDefault();
         var id = $(this).attr("data-id");
         var title = $("#title").val().trim();
@@ -35,6 +49,6 @@ $(document).ready(function () {
         }).then(function(data){
             console.log(data);
         })
-    })
+    });
 
 });
